2222af36322f5287457a401e38ccb6592f0df4bc
FuzzyImages <mursa.artdragons@gmail.com>
2024-12-17 18:59:30 -0800

    Added Word counter

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
+    book = "books/frankenstein.txt"
+    text = get_book(book)
+    #num_words = word_count(text)
+    #print(f"There are {num_words} in this book.")
+    test_type(text)
+
+
+def word_count(text):
+
+    words = text.split()
+    return(len(words))
+
+def test_type(text):
+    print(type(text))
+
+
+def get_book(book):
+
+    with open(book) as f:
+        text = f.read()
+    return text
 
 main()

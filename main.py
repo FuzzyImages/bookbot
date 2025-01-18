a6753ab39211f5028212a72d1c6dd13378eda3fb
FuzzyImages <mursa.artdragons@gmail.com>
2025-01-17 18:55:29 -0800

    refresher

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ def main():
     book = "books/frankenstein.txt"
     text = get_book(book)
     num_words = word_count(text)
+    print("--- Begin report of {book} ---\n")
     print(f"There are {num_words} in this book.")
-    #test_type(text)
+    print(f"report")
 
 
 def word_count(text):
 
     words = text.split()
     return(len(words))
+
 
 def test_type(text):
     print(type(text))

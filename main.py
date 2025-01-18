ec44e4508b92795b75ec59f896c36a5e0c149783
FuzzyImages <mursa.artdragons@gmail.com>
2025-01-17 19:47:18 -0800

    "Added Character Count Function."

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ def main():
     num_words = word_count(text)
     print("--- Begin report of {book} ---\n")
     print(f"There are {num_words} in this book.")
-    print(f"report")
+    character_count = count_characters(text)
+    print(character_count)
+    
+    print(f"report END")
 
 
 def word_count(text):
@@ -12,6 +15,17 @@ def word_count(text):
     words = text.split()
     return(len(words))
 
+
+def count_characters(text):
+
+    characters = {}
+    for character in text:
+        character = character.lower()
+        if character in characters:
+            characters[character] += 1
+        else:
+            characters[character] = 1
+    return characters
 
 def test_type(text):
     print(type(text))

cd75496f52349b113bc8dc253f7e96b2c8252015
FuzzyImages <mursa.artdragons@gmail.com>
2025-01-17 21:11:25 -0800

    "Finished Report Print function"

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ def main():
     book = "books/frankenstein.txt"
     text = get_book(book)
     num_words = word_count(text)
-    print("--- Begin report of {book} ---\n")
-    print(f"There are {num_words} in this book.")
-    character_count = count_characters(text)
-    print(character_count)
-    
-    print(f"report END")
+    print(f"--- Begin report of {book} ---\n")
+    print(f"There are {num_words} in this book.\n")
+    character_report(count_characters(text))
+    print(f"\nend report")
 
 
 def word_count(text):
@@ -27,6 +25,28 @@ def count_characters(text):
             characters[character] = 1
     return characters
 
+
+def character_report(character_dict):
+
+    character_list = []
+
+    for character in character_dict:
+        if character.isalpha():
+            new_char = {}
+            new_char["character"] = character
+            new_char["num"] = character_dict[character]
+            character_list.append(new_char)
+
+    character_list.sort(reverse=True, key=sort_on)
+
+    for character in character_list:
+        print(f"The {character["character"]} character was found {character["num"]} times")
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
 def test_type(text):
     print(type(text))
 
@@ -36,5 +56,6 @@ def get_book(book):
     with open(book) as f:
         text = f.read()
     return text
+
 
 main()

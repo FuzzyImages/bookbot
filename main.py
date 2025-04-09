43b29ab1fb57a761cfbb71dbda1cb0396ae565d5
FuzzyImages <mursa.artdragons@gmail.com>
2025-04-08 17:53:55 -0700

    "Added get_book_text function."

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+
+def main():
+    book = "books/frankenstein.txt"
+
+    get_book_text(book)
+
+
+def get_book_text(book):
+    with open(book) as b:
+        text = b.read() 
+    print(text)
+
+
+if __name__ == "__main__":
+    main()
